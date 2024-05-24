87da43415aae5266a2e84757a376eaf8c827f4e9
Dev Maurya <105772302+DevvMaurya@users.noreply.github.com>
2024-05-24 12:47:14 +0530

    Delete Assistant directory

--- Assistant/commanline_access.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def run_command(command):
-    subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # output, error = process.communicate()
-    return 
